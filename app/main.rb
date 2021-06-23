4c8c6639b8711066347ce3eb7b05d46d08db423f
Eric-Guo <eric.guocz@gmail.com>
2021-06-23 13:36:10 +0800

    Using hash form to draw the labels
    https://dragonruby.redwoodjs.cn/#---rendering-a-label-as-a--hash-

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,6 @@
-def tick args
-  args.outputs.labels << [80, 60, "Hello World",
-                  -5, 0, 200, 050, 100,   225]
+def tick(args)
+  args.outputs.labels << {
+    x: 80, y: 60, text: 'Hello World',
+    size_enum: -5, alignment_enum: 0,
+    r: 200, g: 50, b: 100, a: 225 }
 end
