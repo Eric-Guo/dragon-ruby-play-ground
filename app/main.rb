42b5b41f5a64a3a49a3bde2c9d57f1c738dc5b95
Eric-Guo <eric.guocz@gmail.com>
2021-07-01 08:28:05 +0800

    Using counts as Borders, Solids & Overview

--- app/main.rb
+++ app/main.rb
@@ -1,31 +1,18 @@
+$gtk.reset
 def tick args
-  # Often it is necessary to play with the
-  # coordinates to get things in their
-  # desired locations. We shall create a
-  # couple variables to assist in this process.
-  board_x = 50
-  board_y = 60
-  board_w = 20
-  board_h = 20
+  ticks = args.state.tick_count
+  args.outputs.labels << [64, 70, ticks, -5, 1, 0, 160,
+  0, 255]
+  args.outputs.lines << [37, 55, 86, 73, 100, 100, 100, 255]
+  args.outputs.lines << [37, 74, 86, 56, 50, 50, 50, 255]
 
-  # Boarders and solids are like lines, but they
-  # start at the desired coordinate and then have
-  # a width and height. X and Y are the coordinates.
-  # W and H are width and height.
-  # Similarly to the other expressions
-  # we also have alignment color and transparency.
+  board_x = 36
+  board_y = 54
+  board_w = 52
+  board_h = 21
 
-  #                        X         Y
-  args.outputs.borders << [board_x,  board_y,
-  #  WIDTH     HEIGHT   RED  GREEN   BLUE  ALPHA
-     board_w,  board_h, 0,   0,      200,  255]
-
-  # solids are organized pretty much the same
-  # way as boarders. The only difference is
-  # that they portray a solid color.
+  args.outputs.borders << [board_x, board_y, board_w,
+  board_h, 0, 0, 200,  255 ]
   args.outputs.solids << [board_x+1, board_y+1,
   board_w-2, board_h-2, 35, 0, 50, 255]
-
-  # As before, you can play around with the values
-  # to see how things in the window change
 end
