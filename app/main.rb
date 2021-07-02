598667e6d6a648ddca68c0b665951d60985b2c52
Eric-Guo <eric.guocz@gmail.com>
2021-07-02 21:43:05 +0800

    DragonRuby State Tutorial
    https://www.bilibili.com/video/BV13V411r784

--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,21 @@
-$gtk.reset
 def tick args
-  ticks = args.state.tick_count
-  args.outputs.labels << [64, 70, ticks, -5, 1, 0, 160,
-  0, 255]
-  args.outputs.lines << [37, 55, 86, 73, 100, 100, 100, 255]
-  args.outputs.lines << [37, 74, 86, 56, 50, 50, 50, 255]
+  args.state.x ||= 60
+  args.state.y ||= 250
+  args.state.w ||= 50
+  args.state.h ||= 50
+  args.state.r ||= 0
+  args.state.g ||= 0
+  args.state.b ||= 0
+  args.state.speed ||= 10
 
-  board_x = 36
-  board_y = 54
-  board_w = 52
-  board_h = 21
+  args.outputs.solids << [args.state.x, args.state.y,
+    args.state.w, args.state.h,
+    args.state.r, args.state.g, args.state.b]
 
-  args.outputs.borders << [board_x, board_y, board_w,
-  board_h, 0, 0, 200,  255 ]
-  args.outputs.solids << [board_x+1, board_y+1,
-  board_w-2, board_h-2, 35, 0, 50, 255]
+  args.state.x = lerp(0, 1200, 
+    (Math.sin(args.state.speed * args.state.tick_count / 200) + 1) / 2)
+end
+
+def lerp(start, stop, step)
+  (1.0 - step) * start + step * stop
 end
