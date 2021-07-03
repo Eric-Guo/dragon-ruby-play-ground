13de767d2ad17564af33de2993f6f8e2ef99fd39
Eric-Guo <eric.guocz@gmail.com>
2021-07-03 09:10:56 +0800

    Sprite hashes form.

--- app/main.rb
+++ app/main.rb
@@ -1,41 +1,35 @@
 def tick args
-  # Wow that's a big sprite!
-
-  # Sprites can act as a character, npc, building,
-  # wall, bullet, or basically anything graphical
-  # that you want them to be.
-  # Its easy to make a simple interface using
-  # lines, borders, and solids, but sprites will
-  # fill your game with colors and fantastic
-  # moving parts.
-
-  # Lets start with a simple sprite such as a circle:
-  SIZE = 100
-  # Declaring size helps us manipulate it later.
-  #                         X    Y    W      H
-  args.outputs.sprites << [ 20,  20,  SIZE,  SIZE,
-  # Location/Path
-  'sprites/circle/black.png',
-  # Angle  Alpha  Red    Green  Blue
-    45,    200,   10,    230,   15]
-  # Important to note here is the back slash is
-  # used for the location of the png. Different
-  # systems work differently when tracing a location.
-  # The gtk uses back slashes for location.
-  # It's important to use the correct path,
-  # otherwise... well you will see ;)
-
-  # If we use the method from above we have the
-  # sprites X, and Y coordinates,then the Length
-  # and With in pixels. Following this, we have
-  # the location of the file of the sprite.
-  # Then the Angle, which rotates counter clockwise
-  # starting at 3 O'clock. We use degrees instead
-  # of radians, because...
-  # Well it's so much more intuitive and easy
-  # isn't it? Following the angle is the 3 color
-  # saturation's red, green, and blue,
-  # and last of all is the alpha value.
-
-  # Sprites will continue on the next step.
+  # There are many extra parts to hashes some are
+  # self explanatory others might seem strange
+  # Test them to see what they do.
+  # We do not have enough time this tutorial
+  # to explore them, but future tutorials will.
+  args.outputs.sprites << {
+    x: 80,
+    y: 40,
+    w: 10,
+    h: 10,
+    path: "sprites/circle/black.png",
+    angle: 0,
+    a: 255,
+    r: 255,
+    g: 255,
+    b: 255,
+    # These we wont be touching in this tutorial,
+    # but they are extremely useful. \/ \/ \/
+    source_x:  0,
+    source_y:  0,
+    source_w: -1,
+    source_h: -1,
+    flip_vertically: false,
+    flip_horizontally: false,
+    angle_anchor_x: 0.5,
+    angle_anchor_y: 1.0
+  }
+  # Whats this checkers pattern?
+  # This is what happens when your path doesnt
+  # lead to a file or destination without a
+  # compatible file. No sprite exists of the
+  # designated name in the designated path,
+  # so it outputs a place holder.
 end
