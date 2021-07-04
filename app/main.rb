09b5698bcdae3478dad552d1fce0baabc04a510b
Eric-Guo <eric.guocz@gmail.com>
2021-07-04 11:20:00 +0800

    Sprites Part 6: Wrapping up Sprites

--- app/main.rb
+++ app/main.rb
@@ -1,21 +1,38 @@
 $gtk.reset
 def tick args
+  # Now, if we want our sprite to wrap we need
+  # simply to have the sprites coordinates
+  # change to the opposite side of the screen
+  # when it goes out of bounds or off
+  # the screen. This can also allow you to
+  # make barriers too as we have seen.
+  screen_bound_y = 90
+  screen_bound_x = 160
+  if  args.state.sprite_y <= 0
+    # If the circle y coordinate is less than
+    # the bottom of the screen it will relocate
+    # the sprite to the top of the screen.
+    args.state.sprite_y= screen_bound_y
+  elsif  args.state.sprite_y >= screen_bound_y
+    args.state.sprite_y = 1
+  elsif  args.state.sprite_x <= 0
+    args.state.sprite_x = screen_bound_x
+  elsif  args.state.sprite_x >= screen_bound_x
+    args.state.sprite_x = 1
+  end
+  # Down here we have the extra code we have
+  # already discussed during this tutorial but
+  # is necessary for this part of the tutorial
   if !args.state.sprite_x
     args.state.sprite_x = 120
     args.state.sprite_y = 80
     args.state.sprite_size = 10
     args.state.sprite_movement_speed = 1
-    # Here is the new direction variable
     args.state.direction = 90
   end
-  args.outputs.sprites << [args.state.sprite_x,
-  args.state.sprite_y, args.state.sprite_size,
-  args.state.sprite_size, 'sprites/circle/black.png',
-  args.state.direction]
-  # Now that we have an extra variable to assign
-  # when  a key is pressed we will change the
-  # format of our if statements.We can have our
-  # sprite look in the direction it is traveling.
+  args.outputs.sprites << [args.state.sprite_x, args.state.sprite_y,
+  args.state.sprite_size, args.state.sprite_size,
+  'sprites/circle/black.png', args.state.direction]
   if args.inputs.keyboard.d
     args.state.sprite_x += args.state.sprite_movement_speed
     args.state.direction = 0
@@ -32,27 +49,4 @@ def tick args
     args.state.sprite_y -= args.state.sprite_movement_speed
     args.state.direction = 270
   end
-  # Here we make the bounds equivalent to whatever
-  # our desired boundary numbers are.
-  screen_bound_y = 90
-  screen_bound_x = 160
-  # We could also set up lower bounds for our
-  # sprite but lets stick with zero for now.
-  if  args.state.sprite_y <= 0
-    # If the circle y coordinate is less than
-    # the bottom of the screen it will relocate
-    # the sprite back to where it was before
-    # it tried to leave the screen
-    args.state.sprite_y= 1
-  elsif  args.state.sprite_y >= screen_bound_y
-    args.state.sprite_y = screen_bound_y-1
-  elsif  args.state.sprite_x <= 0
-    args.state.sprite_x = 1
-  elsif  args.state.sprite_x >= screen_bound_x
-    args.state.sprite_x = screen_bound_x-1
-  end
-  # We can shift the boundaries to whatever
-  # we might desire. We could even create an area
-  # inside the bounds that we can not move through.
-  # This could be a wall, a tree, or even an npc.
 end
