c7aa08243810c517f7589579ea3de5de950743da
Eric-Guo <eric.guocz@gmail.com>
2021-07-13 10:07:19 +0800

    https://twitter.com/HIRORB3/status/1386850536041836545

--- app/main.rb
+++ app/main.rb
@@ -1,11 +1,4 @@
 def tick a
-$t+=1
-srand 1
-16.map do
-  x,y=2.map{rand 1e3}
-  h=1+rand(75)
-  g=(rand+3.6*$t*(0.5-rand)).to_i
-  c=[15,105,160].sample
-  0.step(800,20){|j|a.outputs.solids<<[j,-j].map{|n|[x+g.cos*n,y+g.sin*n,h,h,c]}}
-end
+O=a.outputs
+(O[A=:a];O[B=:b];O[:p].solids<<[S=[0,0,W=1280,H=720],[F=255]*3])if 3>$t+=2;A,B=B,A;O[A].sprites<<[a.sprites<<[S,B],[S,:p,0,V=40,0,0,0],32.map_with_ys(18){|x,y|[V*x,20*y+V*($t+x*V).sin-V,V,V,:p,$t,F,c=(V*x+$t).clamp_wrap(0,F),2*c,3*c]}]
 end
