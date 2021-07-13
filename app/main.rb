945868a9c74ce9abeba5d70824d87591125e312a
Eric-Guo <eric.guocz@gmail.com>
2021-07-13 09:57:36 +0800

    https://twitter.com/Groteskly/status/1414605997490978816

--- app/main.rb
+++ app/main.rb
@@ -1,9 +1,7 @@
-def tick args
-  if args.inputs.keyboard.key_down.enter
-    args.gtk.notify_extended! message: "message to show", # message to show
-                              durations: 120,             # how many ticks to show the message
-                              env:       :prod,           # by default, notifications only happend in :dev
-                              overwrite: true             # if this value is true, the notification will
-                                                          # shown even if another one is currently in progress
-  end
+def tick a
+O||=(C,D=0,1;a.outputs)
+C,D=D,C
+O[0]if 2>$t+=1
+a.sprites<<(O[C].sprites<<[a.solids<<[$grid.rect],D,1,F=300])
+O[C].primitives<<[[600+($t*20).sin*90,F+($t*20).cos*90,'. . .',10,0,F,F,F,150].label,[611,331,58,58].solid]
 end
