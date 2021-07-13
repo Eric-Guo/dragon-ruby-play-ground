4a66755b4f833d728f0e5ba5e8bc2abf6bf2a0a2
Eric-Guo <eric.guocz@gmail.com>
2021-07-13 10:15:55 +0800

    cube rotate.
    https://twitter.com/kotaweaver/status/1371793140785553418

--- app/main.rb
+++ app/main.rb
@@ -1,9 +1,16 @@
-def tick a
-$k=!$k?(O,M,B=a.outputs,a.mouse,:b
-O[Q=:a]
-O[:c].solids<<J=[0,0,E=1280,F=720]
-O[:d].sprites<<(0..90).map{|i|[g=20,g,g,g,:c,i]}):(O[B].sprites<<[M.x-30,M.y-30,E,F,:d] if M.button_left
-O[B].sprites<<[J,B=(B==Q)?:b:Q]
-a.sprites<<[J,B])
-O[Q].w=1 if M.button_right
+def tick(a)
+  o = a.outputs
+  o.background_color = [0] * 3
+  27.times do |i|
+    v = i + a.state.tick_count / 60
+    s = Math.sin(v)
+    s2 = 2 * Math.sin(v + 1)
+    p = (0..8).map do |n|
+      z = ((n / 4).to_i % 2 - 7 + s) / 600
+      [(n % 2 - 5 + i % 9) / z + 640, ((n / 2).to_i % 2 + s2) / z + 360]
+    end
+    o.lines << p.combination(2).map do |k, l|
+      [*k, *l, i * 36, (i * s * 9), (i * s2 * 9)]
+    end
+  end
 end
