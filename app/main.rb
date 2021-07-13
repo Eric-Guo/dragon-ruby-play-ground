0dd1090349d7af35d0b70e28678ca94e5e2495b8
Eric-Guo <eric.guocz@gmail.com>
2021-07-13 10:11:35 +0800

    https://twitter.com/Groteskly/status/1375841823361490944

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,10 @@
-$t = 0
+$k = nil
 def tick a
-O=a.outputs
-(O[A=:a];O[B=:b])if 2>$t+=1
-A,B=B,A
-a.sprites<<(O[A].sprites<<[a.solids<<[0,0,1280,720],B,45,c=255])
-O[A].primitives<<[600+$t.sin*25,d=300+$t.cos*25,$t.cos*10,$t.sin*100,r=(Math.atan($t.cos)*d),r+$t.sin*c].solid
+$k=!$k?(O,M,B=a.outputs,a.mouse,:b
+O[Q=:a]
+O[:c].solids<<J=[0,0,E=1280,F=720]
+O[:d].sprites<<(0..90).map{|i|[g=20,g,g,g,:c,i]}):(O[B].sprites<<[M.x-30,M.y-30,E,F,:d] if M.button_left
+O[B].sprites<<[J,B=(B==Q)?:b:Q]
+a.sprites<<[J,B])
+O[Q].w=1 if M.button_right
 end
