acfc7fafbd0c0f86a2dd1eac074bb3a3842264dd
Eric-Guo <eric.guocz@gmail.com>
2021-07-17 16:33:04 +0800

    bounding square

--- app/main.rb
+++ app/main.rb
@@ -1,34 +1,71 @@
+# http://fiddle.dragonruby.org/?tutorial=https://gist.github.com/amirrajan/83c368bfc4f153abdfba995458d8943a
+
 def tick(args)
-  args.state.player.x ||= 1280/2
-  args.state.player.y ||= 720/2
-  args.state.player.w ||= 16
-  args.state.player.h ||= 16
-  args.outputs.sprites << {
-    x: args.state.player.x,
-    y: args.state.player.y,
-    w: args.state.player.w,
-    h: args.state.player.h,
-    path: 'sprites/square/green.png'
-  }
-  if args.inputs.keyboard.right
-    args.state.player.x += 2
-  elsif args.inputs.keyboard.left
-    args.state.player.x -= 2
+  defaults args
+  render args
+  input args
+  calc args
+end
+
+def defaults(args)
+  args.state.colors       = %i[red orange green blue indigo violet]
+  args.state.box.size     = 50
+  args.state.box.speed    = 2
+  args.state.box.x      ||= args.grid.w.half - args.state.box.size.half
+  args.state.box.y      ||= args.grid.h.half - args.state.box.size.half
+  args.state.box.dx     ||= [1, -1].sample
+  args.state.box.dy     ||= [1, -1].sample
+  args.state.box.status ||= :moving
+  set_box_color args    unless args.state.box.color
+end
+
+def render(args)
+  args.outputs.sprites << [
+    args.state.box.x,
+    args.state.box.y,
+    args.state.box.size,
+    args.state.box.size,
+    "sprites/square/#{args.state.box.color}.png"
+  ]
+end
+
+def input(args)
+  return unless args.inputs.keyboard.key_down.space
+
+  args.state.box.status = if args.state.box.status == :moving
+                            :stopped
+                          else
+                            :moving
+                          end
+end
+
+def calc(args)
+  return unless args.state.box.status == :moving
+
+  args.state.box.x += args.state.box.dx * args.state.box.speed
+  args.state.box.y += args.state.box.dy * args.state.box.speed
+
+  if (args.state.box.x + args.state.box.size) > args.grid.w
+    args.state.box.x  = args.grid.w - args.state.box.size
+    args.state.box.dx = -1
+    set_box_color args
+  elsif args.state.box.x.negative?
+    args.state.box.x  = 0
+    args.state.box.dx = 1
+    set_box_color args
   end
-  if args.inputs.keyboard.up
-    args.state.player.y += 2
-  elsif args.inputs.keyboard.down
-    args.state.player.y -= 2
+
+  if (args.state.box.y + args.state.box.size) > args.grid.h
+    args.state.box.y  = args.grid.h - args.state.box.size
+    args.state.box.dy = -1
+    set_box_color args
+  elsif args.state.box.y.negative?
+    args.state.box.y  = 0
+    args.state.box.dy = 1
+    set_box_color args
   end
-  # make sure the player's x and y position
-  # are clamped to the the grid's minimum and
-  # maximum width and height
-  args.state.player.x = args.state.player.x.clamp(
-    args.grid.left,
-    args.grid.right - args.state.player.w
-  )
-  args.state.player.y = args.state.player.y.clamp(
-    args.grid.bottom,
-    args.grid.top - args.state.player.h
-  )
+end
+
+def set_box_color(args)
+  args.state.box.color = (args.state.colors - [args.state.box.color]).sample
 end
