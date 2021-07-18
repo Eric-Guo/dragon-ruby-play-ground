fb9d53c7cab9dda0cf07a70b69721b645999538b
Eric-Guo <eric.guocz@gmail.com>
2021-07-18 14:29:07 +0800

    Adding Gravity

--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,6 @@
 def tick(args)
+  # define gravity
+  args.state.gravity    = -1
   # place the player in the center of the
   # grid horizontally and on the floor (y = 0)
   args.state.player.x    ||= args.grid.w.half
@@ -9,10 +11,13 @@ def tick(args)
   args.state.player.dy   ||= 0
   # define jump power
   args.state.jump.power = 10
+  # keep track of the player's current action
+  args.state.player.action ||= :standing
 
   # set dy equal to jump power if space is pressed
   if args.inputs.keyboard.key_down.space
     args.state.player.dy = args.state.jump.power
+    args.state.player.action = :jumping
   end
 
   # reset the player's y and dy if r
@@ -22,8 +27,17 @@ def tick(args)
     args.state.player.dy = 0
   end
 
-  # increase the player's y by dy every frame
-  args.state.player.y += args.state.player.dy
+  # apply gravity and dy if the player is jumping
+  if args.state.player.action == :jumping
+    args.state.player.y  += args.state.player.dy
+    args.state.player.dy += args.state.gravity
+  end
+  # set the action to :standing when
+  # the player hits the ground
+  if args.state.player.y < 0
+    args.state.player.y      = 0
+    args.state.player.action = :standing
+  end
 
   # render the player on the screen
   # taking into consideration the player's
