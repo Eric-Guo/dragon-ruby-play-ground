7232af5b223898e32e8fafbd6bd27323a2eb65db
Eric-Guo <eric.guocz@gmail.com>
2021-07-18 14:25:43 +0800

    Zero Gravity Jump

--- app/main.rb
+++ app/main.rb
@@ -4,6 +4,27 @@ def tick(args)
   args.state.player.x    ||= args.grid.w.half
   args.state.player.y    ||= 0
   args.state.player.size ||= 10
+
+  # define dy for the player
+  args.state.player.dy   ||= 0
+  # define jump power
+  args.state.jump.power = 10
+
+  # set dy equal to jump power if space is pressed
+  if args.inputs.keyboard.key_down.space
+    args.state.player.dy = args.state.jump.power
+  end
+
+  # reset the player's y and dy if r
+  # is pressed on the keyboard
+  if args.inputs.keyboard.key_down.r
+    args.state.player.y  = 0
+    args.state.player.dy = 0
+  end
+
+  # increase the player's y by dy every frame
+  args.state.player.y += args.state.player.dy
+
   # render the player on the screen
   # taking into consideration the player's
   # size to center them
