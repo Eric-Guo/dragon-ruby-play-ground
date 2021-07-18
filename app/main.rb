f316b5d7bfa2b23abeea65d808e0eaf57e9f731d
Eric-Guo <eric.guocz@gmail.com>
2021-07-18 20:28:01 +0800

    Final Step: Short Hopping
    '

--- app/main.rb
+++ app/main.rb
@@ -6,18 +6,38 @@ def tick(args)
   args.state.player.x    ||= args.grid.w.half
   args.state.player.y    ||= 0
   args.state.player.size ||= 10
-
-  # define dy for the player
   args.state.player.dy   ||= 0
-  # define jump power
-  args.state.jump.power = 10
-  # keep track of the player's current action
-  args.state.player.action ||= :standing
 
-  # set dy equal to jump power if space is pressed
-  if args.inputs.keyboard.key_down.space
-    args.state.player.dy = args.state.jump.power
+  args.state.player.action ||= :jumping
+  args.state.jump.power           = 5
+  args.state.jump.increase_frames = 15
+  args.state.jump.increase_power  = 1
+
+  args.outputs.sprites << {
+    x: args.state.player.x -
+       args.state.player.size.half,
+    y: args.state.player.y,
+    w: args.state.player.size,
+    h: args.state.player.size,
+    path: 'sprites/square/red.png'
+  }
+
+  if args.inputs.keyboard.key_down.space && (args.state.player.action == :standing)
     args.state.player.action = :jumping
+    args.state.player.dy = args.state.jump.power
+    current_frame = args.state.tick_count
+    args.state.player.action_at = current_frame
+  end
+
+  if args.inputs.keyboard.key_held.space
+    is_jumping = args.state.player.action == :jumping
+    time_of_jump = args.state.player.action_at
+    jump_elapsed_time = time_of_jump.elapsed_time
+    time_allowed = args.state.jump.increase_frames
+    if is_jumping && jump_elapsed_time < time_allowed
+      power_to_add = args.state.jump.increase_power
+      args.state.player.dy += power_to_add
+    end
   end
 
   # reset the player's y and dy if r
@@ -32,22 +52,11 @@ def tick(args)
     args.state.player.y  += args.state.player.dy
     args.state.player.dy += args.state.gravity
   end
+
   # set the action to :standing when
   # the player hits the ground
   if args.state.player.y < 0
     args.state.player.y      = 0
     args.state.player.action = :standing
   end
-
-  # render the player on the screen
-  # taking into consideration the player's
-  # size to center them
-  args.outputs.sprites << {
-    x: args.state.player.x -
-       args.state.player.size.half,
-    y: args.state.player.y,
-    w: args.state.player.size,
-    h: args.state.player.size,
-    path: 'sprites/square/red.png'
-  }
 end
