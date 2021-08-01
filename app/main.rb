961f0e904a4d903989d04467c3e2e639ea44883c
Ruby GSY <rubygsy@gmail.com>
2021-08-01 09:54:47 +0800

    6 square moving

--- app/main.rb
+++ app/main.rb
@@ -1,15 +1,49 @@
-def tick(args)
-  [
-    [0, 720, 1280, 0, 0, 0, 0, 255],
-    [1280, 720, 0, 0, 0, 0,      0, 255],
-    [1280 / 2, 720, 1280 / 2, 0, 0, 0, 0, 255],
-    [0, 720 / 2, 1280, 720 / 2, 0, 0, 0, 255],
+def tick args
+  ticks = args.state.tick_count
 
-    [1280 / 2, 720,   1280, 720 / 2, 0, 0, 0, 255],
-    [1280, 720 / 2,   1280 / 2, 0, 0, 0, 0, 255],
-    [1280 / 2, 0, 0, 720 / 2, 0, 0, 0, 255],
-    [0,  720 / 2, 1280 / 2, 720, 0, 0, 0, 255]
-  ].each do |line|
-    args.outputs.lines << line
-  end
+  board_x = 50
+  board_y = 60
+  board_w = 50
+  board_h = 50
+
+  args.outputs.borders << [board_x,  board_y,
+     board_w,  board_h, 0,   0,      0,  255]
+
+  args.outputs.solids << [board_x+6, board_y+6,
+    board_w-12, board_h-12, 35, 0, 50, 255]
+
+
+  args.outputs.borders << [board_x+ticks*2,  board_y+ticks,
+    board_w,  board_h, 200,   0,      0,  255]
+
+  args.outputs.solids << [board_x+6+ticks*2, board_y+6+ticks,
+    board_w-12, board_h-12, 200, 0, 0, 255]
+
+
+  args.outputs.borders << [board_x+ticks,  board_y+ticks,
+    board_w,  board_h, 0,   200,      0,  255]
+
+  args.outputs.solids << [board_x+6+ticks, board_y+6+ticks,
+    board_w-12, board_h-12, 0, 200, 0, 255]
+
+
+  args.outputs.borders << [board_x,  board_y+ticks,
+    board_w,  board_h, 0,   0,      200,  255]
+
+  args.outputs.solids << [board_x+6, board_y+6+ticks,
+    board_w-12, board_h-12, 0, 0, 200, 255]
+
+
+  args.outputs.borders << [board_x+ticks,  board_y,
+    board_w,  board_h, 0,   100,      100,  255]
+
+  args.outputs.solids << [board_x+6+ticks, board_y+6,
+    board_w-12, board_h-12, 0, 100, 100, 255]
+
+
+  args.outputs.borders << [board_x+ticks*2,  board_y,
+    board_w,  board_h, 100,   0,      200,  255]
+
+  args.outputs.solids << [board_x+ticks*2+6, board_y+6,
+    board_w-12, board_h-12, 100, 0, 200, 255]
 end
