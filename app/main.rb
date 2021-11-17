9a3849f92188018c9f5ad6a49f8b41e605acaf56
Eric-Guo <eric.guocz@gmail.com>
2021-11-17 16:24:33 +0800

    20 minute snake game with DragonRuby GTK
    https://gist.github.com/marcheiligers/173634708371635e257dfba84c39f774

--- app/main.rb
+++ app/main.rb
@@ -1,15 +1,86 @@
-# frozen_string_literal: true
-$t=0
-TICK do |a|
-  if (t = $t += 1) % 270 == 1
-    (X = rand 1280
-     Y = rand 720
-     A = rand(3) * r = 90
-     B = rand(r) + 50
-     C = [[q = 220, r, r], [q, s = 150, 5], [r, r, q], [r, s, s]].sample)
+GRID_SIZE = 20
+GRID_WIDTH = $gtk.args.grid.w / GRID_SIZE
+GRID_HEIGHT = $gtk.args.grid.h / GRID_SIZE
+
+class Snake
+  def initialize
+    @direction = :right
+    @speed = 20
+    @logical_x = GRID_WIDTH / 2
+    @logical_y = GRID_HEIGHT / 2
+    @length = 0
+    @body = []
+    @fruit = random_fruit
   end
-  C.x += u = t.sin
-  C.y += v = t.cos
-  B.times { |n| sld!(X + (w = A + t).sin * n, Y + w.cos * n, (1 + u) * 30 + 5, (1 + v) * 30 + 5, C) }
-  (h = a.outputs[:scene]).clear_before_render, h.w, h.h = p
+
+  def tick(args)
+    handle_input(args)
+    handle_move(args)
+    handle_fruit
+  end
+
+  def handle_input(args)
+    inputs = args.inputs
+
+    case
+    when inputs.right then @direction = :right
+    when inputs.left then @direction = :left
+    when inputs.up then @direction = :up
+    when inputs.down then @direction = :down
+    end
+  end
+
+  def handle_move(args)
+    return unless args.tick_count % @speed == 0
+
+    if @length > @body.length
+      @body << head
+    elsif @body.length > 0
+      @body.shift
+      @body << head
+    end
+
+    case @direction
+    when :right then @logical_x += 1
+    when :left then @logical_x -= 1
+    when :up then @logical_y += 1
+    when :down then @logical_y -= 1
+    end
+  end
+
+  def handle_fruit
+    return unless head == @fruit
+
+    @length += 1
+    @speed -= 1
+    @fruit = random_fruit
+  end
+
+  def random_fruit
+    [
+      rand(GRID_WIDTH),
+      rand(GRID_HEIGHT)
+    ]
+  end
+
+  def to_p
+    [section(head, { r: 0, g: 0, b: 0 })] +
+      @body.map { |pos| section(pos) } +
+      [section(@fruit, { r: 200, g: 12, b: 12 })]
+  end
+
+  def section(pos, color = { r: 12, g: 100, b: 12 })
+    { x: pos.x * GRID_SIZE, y: pos.y * GRID_SIZE, w: GRID_SIZE, h: GRID_SIZE }.merge(color).solid!
+  end
+
+  def head
+    [@logical_x, @logical_y]
+  end
+end
+
+$snake = Snake.new
+
+def tick(args)
+  $snake.tick(args)
+  args.outputs.primitives << $snake.to_p
 end
