07412a84b4df91acf98aa313345b1644b653635a
Eric Guo <eric@cloud-mes.com>
2021-11-28 19:53:32 +0800

    Using static_* won't clean can be mutated by reference.

--- app/main.rb
+++ app/main.rb
@@ -4,14 +4,10 @@ def tick(args)
   args.outputs.background_color = [63, 63, 63]
   args.outputs.debug << [10, 720, "FPS #{args.gtk.current_framerate.round}"].label
 
-  clear_target = args.state.tick_count.zero? || args.inputs.keyboard.key_down.space || args.inputs.mouse.click
-  args.render_target(:accumulation).clear_before_render = clear_target
-  unless args.state.tick_count.zero?
-    args.render_target(:accumulation).solids << [args.inputs.mouse.x, args.inputs.mouse.y, 100, 100, rand(255),
-                                                 rand(255), rand(255)]
-    args.render_target(:accumulation).borders << [args.inputs.mouse.x, args.inputs.mouse.y, 100, 100, rand(255),
-                                                  rand(255), rand(255)]
-  end
+  $gtk.reset if args.inputs.keyboard.key_down.space || args.inputs.mouse.click
 
-  args.outputs.sprites << [0, 0, 1280, 720, :accumulation]
+  return if args.state.tick_count.zero?
+
+  args.outputs.static_solids << [args.inputs.mouse.x, args.inputs.mouse.y, 100, 100, rand(255), rand(255), rand(255)]
+  args.outputs.static_borders << [args.inputs.mouse.x, args.inputs.mouse.y, 100, 100, rand(255), rand(255), rand(255)]
 end
