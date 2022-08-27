feac19cfb13825010a029f6a9c770df6b72e3423
Eric-Guo <eric.guocz@gmail.com>
2022-08-27 22:04:22 +0800

    draft version of draw solids.

--- app/main.rb
+++ app/main.rb
@@ -1,86 +1,31 @@
 # frozen_string_literal: true
 
-BOARD_W = 50
-BOARD_H = 50
-
-def box_color(num)
-  [
-    [0, 0, 0, 255],
-    [200, 0, 0, 255],
-    [0, 200, 0, 255],
-    [0, 0, 200, 255],
-    [0, 100, 100, 255],
-    [100, 0, 200, 255],
-    [0, 60, 70, 250],
-    [10, 50, 40, 255],
-    [60, 0, 50, 255]
-  ][num]
-end
-
 def tick(args)
-  ticks = args.state.tick_count
-
-  board_x = 50
-  board_y = 60
-
-  draw_box(args, board_x, board_y, 0, ticks)
-  draw_box(args, board_x, board_y, 1, ticks)
-  draw_box(args, board_x, board_y, 2, ticks)
-  draw_box(args, board_x, board_y, 3, ticks)
-  draw_box(args, board_x, board_y, 4, ticks)
-  draw_box(args, board_x, board_y, 5, ticks)
-  draw_box(args, board_x, board_y, 6, ticks)
-  draw_box(args, board_x, board_y, 7, ticks)
-  draw_box(args, board_x, board_y, 8, ticks)
+  render args
 end
 
-def lerp(start, stop, step)
-  (1.0 - step) * start + step * stop
-end
+def render(args)
+  group = [
+    { w:  20, h: 20, r: 255, g: 0, b: 0, a: 255 },
+    { w:  20, h:  20, r:    0, g:  255, b:    0, a: 255 },
+    { w:  20, h:  20, r:    0, g:  0, b: 255, a: 255 },
+    { w:  20, h:  20, r:    127, g: 127, b: 127, a: 255 }
+  ]
 
-def draw_box(args, board_x, board_y, box_num, ticks)
-  box_color = box_color(box_num)
+  (args.layout.rect_group row: 0, col: 0, dcol: 0.5,
+                          merge: { vertical_alignment_enum: 0, size_enum: -2 },
+                          group: group * 12).into args.outputs.solids
 
-  leap_ticks = lerp(0, 280,
-                    (Math.sin(2 * ticks / 200) + 1) / 2)
+  (args.layout.rect_group row: 1, col: 0, dcol: 0.5,
+                          merge: { vertical_alignment_enum: 0, size_enum: -2 },
+                          group: group.reverse * 12).into args.outputs.solids
 
-  case box_num
-  when 0
-    x = board_x
-    y = board_y
-  when 1
-    x = board_x + leap_ticks * 4
-    y = board_y + leap_ticks
-  when 2
-    x = board_x + leap_ticks * 2
-    y = board_y + leap_ticks
-  when 3
-    x = board_x
-    y = board_y + leap_ticks
-  when 4
-    x = board_x + leap_ticks * 2
-    y = board_y
-  when 5
-    x = board_x + leap_ticks * 4
-    y = board_y
-  when 6
-    x = board_x + leap_ticks * 2
-    y = board_y + leap_ticks * 2
-  when 7
-    x = board_x
-    y = board_y + leap_ticks * 2
-  when 8
-    x = board_x + leap_ticks * 4
-    y = board_y + leap_ticks * 2
-  end
+  (args.layout.rect_group row: 2, col: 0, dcol: 0.5,
+                          merge: { vertical_alignment_enum: 0, size_enum: -2 },
+                          group: group.shuffle(Random.new(1)) * 12).into args.outputs.solids
 
-  args.outputs.borders << [x, y,
-                           BOARD_W, BOARD_H] + box_color
+  (args.layout.rect_group row: 3, col: 0, dcol: 0.5,
+                          merge: { vertical_alignment_enum: 0, size_enum: -2 },
+                          group: group.shuffle(Random.new(2)) * 12).into args.outputs.solids
 
-  draw_solid(args, x, y, box_color)
-end
-
-def draw_solid(args, board_x, board_y, box_color)
-  args.outputs.solids << [board_x + 6, board_y + 6,
-                          BOARD_W - 12, BOARD_H - 12] + box_color
 end
